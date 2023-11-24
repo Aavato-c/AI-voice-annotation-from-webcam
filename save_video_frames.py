cd7158078d522e5271ab807de8df9c375dbfcd58
Aavato-c <80039165+Aavato-c@users.noreply.github.com>
2023-11-24 21:14:34 +0200

    Capture image from webcam

--- save_video_frames.py
+++ save_video_frames.py
@@ -2,6 +2,8 @@ import os
 import logging
 import cv2
 import time
+from cv2 import imshow, waitKey, destroyWindow, imwrite
+
 
 
 logger = logging.getLogger(__name__)
@@ -14,5 +16,26 @@ logging.basicConfig(
     ]
   )
 
-
 vid = cv2.VideoCapture(0)
+
+
+
+result, image = vid.read()
+
+if result: 
+  
+    # showing result, it take frame name and image  
+    # output 
+    imshow("Test", image) 
+  
+    # saving image in local storage 
+    imwrite("Test.png", image) 
+  
+    # If keyboard interrupt occurs, destroy image  
+    # window 
+    waitKey(0) 
+    destroyWindow("Test") 
+  
+# If captured image is corrupted, moving to else part 
+else: 
+    print("No image detected. Please try again") 
