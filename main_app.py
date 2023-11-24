616e9736d5fc7f7ce283e57e24704d8922f0506e
Aavato-c <80039165+Aavato-c@users.noreply.github.com>
2023-11-24 21:11:07 +0200

    We'll pass it to a function

--- main_app.py
+++ main_app.py
@@ -27,9 +27,6 @@ def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
 
-# Path to your image
-image_path = "path_to_your_image.jpg"
-
 # Getting the base64 string
 base64_image = encode_image(image_path)
 
