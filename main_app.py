996a4b0874dab559cd61e4d8f73b20af30f07787
Aavato-c <80039165+Aavato-c@users.noreply.github.com>
2023-11-24 21:34:21 +0200

    Call from main func

--- main_app.py
+++ main_app.py
@@ -29,7 +29,9 @@ def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
 
+# Getting the base64 strin
 def get_description(image_path = IMAGE_PATH):
+  base64_image = encode_image(image_path)
 
   headers = {
     "Content-Type": "application/json",
