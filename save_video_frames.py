a99262d8402ccf0081dfe1ea7cefdb3005cdc485
Aavato-c <80039165+Aavato-c@users.noreply.github.com>
2023-11-24 21:23:02 +0200

    Make a broiler to capture frames

--- save_video_frames.py
+++ save_video_frames.py
@@ -3,6 +3,7 @@ import logging
 import cv2
 import time
 from cv2 import imshow, waitKey, destroyWindow, imwrite
+import datetime
 
 
 
@@ -16,26 +17,26 @@ logging.basicConfig(
     ]
   )
 
-vid = cv2.VideoCapture(0)
+
+
+def getFrames_loop():
+    vid = cv2.VideoCapture(0)
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    hasFrames, image = vid.read()
+    if hasFrames:
+        imshow("Current_image", image) 
+        imwrite("./media/CURRENT.jpg", image,)
+        imwrite(f"./media/archive/IMG_{timestamp}.jpg", image,)
+        destroyWindow("Current_image") 
+        logger.debug("Image saved")
+    else:
+        logger.info("Couldn't get image. We'll wait a sec and try again")
+        time.sleep(3)
+        vid.release()
+        getFrames_loop()
+
+    return image
 
 
 
-result, image = vid.read()
-
-if result: 
   
-    # showing result, it take frame name and image  
-    # output 
-    imshow("Test", image) 
-  
-    # saving image in local storage 
-    imwrite("Test.png", image) 
-  
-    # If keyboard interrupt occurs, destroy image  
-    # window 
-    waitKey(0) 
-    destroyWindow("Test") 
-  
-# If captured image is corrupted, moving to else part 
-else: 
-    print("No image detected. Please try again") 
